2ccf17c0a99108edafebea4ed9677bb6c3cb0c39
Júnior de Oliveira <junior_siq90@hotmail.com>
2021-01-15 19:15:57 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,9 +2,19 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity //informa que classe Category é uma entidade
+@Table(name = "tb_category") //será o nome da tabela no banco
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //id será auto incrementando com essa anotação
 	private Long id;
 	private String name;
 
